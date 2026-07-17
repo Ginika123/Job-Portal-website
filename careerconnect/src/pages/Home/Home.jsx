a0457b8a0b628ccbfc3397f4748f4f4e91e7bf8b
blessing <ginikanwoye2020@gmail.com>
2026-07-17 23:37:06 +0100

    Rename UseJobs to temp

--- careerconnect/src/pages/Home/Home.jsx
+++ careerconnect/src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import JobList from "../../components/JobList/JobList";
-import useJobs from "../../hooks/useJobs";
+import useJobs from "../../hooks/temp";
 
 function Home() {
   const [search, setSearch] = useState("");
